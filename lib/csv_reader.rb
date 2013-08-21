ec8d6a86ca44ed6296ff9e1cff0dcac60a526018
Patricia Estridge <patricia.j.estridge@gmail.com>
2013-08-21 12:27:28 +0100

    changes to CSV file

---
+++ lib/csv_reader.rb
@@ -0,0 +1,42 @@
+class String
+  def underscore
+    self.gsub(/::/, '/').
+    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+    gsub(/([a-z\d])(A-Z)/,'\1_\2').
+    tr("-", "-").
+    downcase
+  end
+end
+
+class CSVReader
+
+  attr_accessor :fname, :headers
+
+  def initialize(filename)
+    @fname = filename
+  end
+
+  def headers=(header_str)
+    @headers = header_str.split(',')
+    @headers.map! do |h|
+      #removes quotes
+      h.gsub!('"', '')
+      #remove new line
+      h.strip!
+      #convert to symbol
+      h.underscore.to_sym
+    end
+  end
+
+  def create_hash(values)
+    h = {}
+    @headers.each_with_index do |header, i|
+    #removes new ines from the value
+    value = values[i].strip.gsub('"', '')
+    h[header] = value unless value.empty
+    end
+    h
+  end
+end
+
+
